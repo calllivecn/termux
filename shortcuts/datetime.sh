ed548ad40ca0ad2b37569ab168ad609e5669ede2
calllivecn <calllivecn@outlook.com>
2019-11-25 22:53:43 +0800

    first commit
    new file:   config/.gitconfig
    new file:   config/bashrc
    new file:   config/git.globalconfig
    new file:   config/profile
    new file:   config/tmux.conf
    new file:   config/vimrc
    new file:   install.sh
    new file:   shortcuts/datetime.sh
    new file:   shortcuts/mc-检查-py
    new file:   shortcuts/say.sh
    new file:   shortcuts/sshd
    new file:   shortcuts/timer.sh
    new file:   termux-backup.sh
    new file:   termux-packages-backup.txt

---
+++ shortcuts/datetime.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# date 2019-11-25 22:51:38
+# author calllivecn <c-all@qq.com>
+set -x
+
+termux-tts-speak "现在是: $(date +%F) $(date +%R)"
---
+++ shortcuts/say.sh
@@ -0,0 +1,5 @@
+termux-tts-speak "我是谁？"
+
+sleep 5
+
+termux-tts-speak "饭煮好了"
