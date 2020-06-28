5b39c0c1f4c51f83a2d467c28f94a0c36e942c5a
calllivecn <calllivecn@outlook.com>
2020-06-28 10:39:07 +0800

    add

---
+++ config/pip-conf.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# date 2020-04-26 11:02:05
+# author calllivecn <c-all@qq.com>
+
+
+# 配置系统源
+#sudo pip3 config --global set global.index-url https://mirrors.aliyun.com/pypi/simple/
+
+# 这是配置当前用户的源
+pip3 config set global.index-url https://mirrors.aliyun.com/pypi/simple/
