9bfc6e15f742bf4c5f94de83a1534ad5c381d029
calllivecn <c-all@qq.com>
2020-06-28 14:30:38 +0800

    update testing

--- init-termux.sh
+++ init-termux.sh
@@ -2,25 +2,32 @@
 # date 2019-11-28 02:35:06
 # author calllivecn <c-all@qq.com>
 
+PROGRAM_DIR="$(dirname $(pwd)/${0})"
+
 
 copy(){
-	if [ -d "$2" ];then
+	local dir="$(dirname ${2})"
+
+	if [ -d "$dir" ];then
 		:
 	else
-		echo "$2 目标目录不存在..." >&2
+		echo "$dir 目标目录不存在..." >&2
+		echo "创建目录 $dir"
+		mkdir -v -p "$dri"
 		return 
 	fi
 
 	if [ -f "$1" ];then
-		install -m0644 "$1" $HOME/
+		cp -v "$1" "$2"
 	else
 		echo "$1 不存在..." >&2
 	fi
 }
 
 copy_dir(){
+
 	if [ -d "$1" ];then
-		echo "$1 目录不存在..." >&2
+		echo "$1 要复制的目录不存在..." >&2
 		return 
 	fi
 
@@ -32,39 +39,36 @@ copy_dir(){
 	fi
 }
 
-copy config/profile $HOME/.profile
-
-copy config/bashrc $HOME/.bashrc
-
-copy config/vimrc $HOME/.vimrc
-
-copy config/git.globalconfig $HOME/.gitconfig
-
-
-copy_dir shortcuts $HOME/.shortcuts
-
-[ -d $HOME/.termux ] || mkdir $HOME/.termux
-
-copy termux.properties
-
-
-if [ -d $HOME/bin ];then
-	copy termux-backup.sh $HOME/bin
-	copy liblock.sh $HOME/bin
-else
-	mkdir -v $HOME/bin
-	copy termux-backup.sh $HOME/bin
-	copy liblock.sh $HOME/bin
-fi
-
-SOURCES=$PREFIX/etc/apt/sources.list
-
-if grep -qE "https://termux.org/" $SOURCES;then
-	mv -v $SOURCES "${SOURCES}-bak"
-	echo "deb https://mirrors.tuna.tsinghua.edu.cn/termux stable main" > $SOURCES
-fi
-
+update_apt_sources(){
+	SOURCES=$PREFIX/etc/apt/sources.list
+	
+	if grep -qE "https://termux.org/" $SOURCES;then
+		mv -v $SOURCES "${SOURCES}-bak"
+		echo "deb https://mirrors.tuna.tsinghua.edu.cn/termux stable main" > $SOURCES
+	fi
+}
 
 #apt update
 
+
+
+CONFIG="${PROGRAM_DIR}/config"
+
+copy $CONFIG/profile $HOME/.profile
+
+copy $CONFIG/bashrc $HOME/.bashrc
+
+copy $CONFIG/vimrc $HOME/.vimrc
+
+copy $CONFIG/git.globalconfig $HOME/.gitconfig
+
+copy_dir ${PROGRAM_DIR}/shortcuts $HOME/.shortcuts
+
+
+[ -d $HOME/.termux ] || mkdir $HOME/.termux
+
+copy ${PROGRAM_DIR}/termux.properties $HOME/.termux/
+
+
+copy_dir $PROGRAM_DIR/bin/ $HOME/
 
