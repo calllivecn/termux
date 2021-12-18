d4d71e1c288a8fe87919d477858273fbea24e852
calllivecn <calllivecn@outlook.com>
2021-12-18 23:11:31 +0800

    测试应该好了

--- android-job-scheduler.sh
+++ android-job-scheduler.sh
@@ -9,7 +9,7 @@ PROGRAM="${0##*/}"
 PROGRAM="${PROGRAM%.sh}"
 DIR="$(dirname ${0})"
 
-PID="${DIR}/${PROGRAM}.pid"
+PIDFILE="${DIR}/${PROGRAM}.pid"
 LOG="${DIR}/${PROGRAM}.log"
 
 # 这种方案不行;安卓里被干掉时，类似kill -9 $pid, 进程收不到。
@@ -26,20 +26,20 @@ LOG="${DIR}/${PROGRAM}.log"
 
 # 这里查看后台进程是否被干掉。
 JobScheduler(){
-	if [ -f "$PID" ];then
-		pidfile="$(cat $PID)"
-		if ps -h -f -p "$PID" 2>/dev/null |grep -v grep |grep -q "$program" ;then
+	if [ -f "$PIDFILE" ];then
+		pid="$(cat $PIDFILE)"
+		if ps -h -f -p "$pid" 2>/dev/null |grep -v grep |grep -q "$program" ;then
 			echo "job scheduler 成功，本次退出... pid:$$"
 			echo "job scheduler 成功，本次退出... pid:$$" >> "$LOG"
 			exit 0
 		else
-			echo "$$" > "$PID"
+			echo "$$" > "$PIDFILE"
 			echo "本次重新启动：$(date +%F-%R) ... pid:$$"
 			echo "本次重新启动：$(date +%F-%R) ... pid:$$" >> "$LOG"
 		fi
 	else
 		# 正常启动
-		echo "$$" > "$PID"
+		echo "$$" > "$PIDFILE"
 		echo "本次正常启动：$(date +%F-%R) ... pid:$$"
 		echo "本次正常启动：$(date +%F-%R) ... pid:$$" >> "$LOG"
 	fi
