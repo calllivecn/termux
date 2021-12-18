bd9af268716285219dce837b40a5befad4bf1129
calllivecn <calllivecn@outlook.com>
2021-12-19 02:23:38 +0800

    小修改

--- bin/apt-proxy.sh
+++ bin/apt-proxy.sh
@@ -3,31 +3,27 @@
 # author calllivecn <c-all@qq.com>
 
 
-PROXY_ADDR="http://192.168.9.1:9999"
+PROXY_ADDR="$(grep -oP '(?<=http_proxy=")(.*)(?=")' $HOME/.config/xiyou)"
 
 URLs="termux.org
-dl.bintray.com"
+dl.bintray.com
+"
 
 APT_OPTION=""
-
-apt_proxy(){
-	for http in $URLs
+for http in $URLs
+do
+	for scheme in http https
 	do
-		for scheme in http https
-		do
-			APT_OPTION="$APT_OPTION -o Acquire::${scheme}::Proxy::${http}=$PROXY_ADDR "
-		done
+		APT_OPTION="$APT_OPTION -o Acquire::${scheme}::Proxy::${http}=$PROXY_ADDR "
 	done
-}
+done
 
-apt_ignore(){
-	for http in $URLs
-	do
-		for scheme in http https
-		do
-			APT_OPTION="$APT_OPTION -o Acquire::${scheme}::Proxy::${http}=$PROXY_IGNORE "
-		done
-	done
+SUDO(){
+	if [ $EUID -ne 0 ];then
+		sudo apt $APT_OPTION "$@"
+	else
+		apt $APT_OPTION "$@"
+	fi  
 }
 
 
@@ -35,14 +31,7 @@ apt_ignore(){
 if [ "$1"x = "debug"x ];then
 	shift
 	echo apt $APT_OPTION "$@"
-
-elif [ "$1"x = "ignore"x ];then
-	shift
-	apt_ignore
-	apt $APT_OPTION "$@"
-
 else
-	apt_proxy
-	apt $APT_OPTION "$@"
+	SUDO "$@"
 fi
 
