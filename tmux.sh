9436c50892b8ac1d3f4984829565f91e0f235642
calllivecn <c-all@qq.com>
2022-05-22 10:39:15 +0800

    save

--- tmux.sh
+++ tmux.sh
@@ -1,1 +1,7 @@
-tmux -S /data/data/com.termux/files/usr/var/run/tmux-10156/default attach
+S="/data/data/com.termux/files/usr/var/run/tmux-10156/default"
+
+if [ -S $S ];then
+	tmux -S $S attach
+else
+	tmux -S $S
+fi
