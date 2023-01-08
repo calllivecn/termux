4f015442f018c14a7fbbf76104670c9ea819172e
calllivecn <c-all@qq.com>
2023-01-09 03:17:55 +0800

    fix read -t 10 text <  不会超时

--- bin/tts.sh
+++ bin/tts.sh
@@ -1,6 +1,12 @@
 #!/data/data/com.termux/files/usr/bin/bash
 # date 2023-01-08 09:05:43
 # author calllivecn <c-all@qq.com>
+
+
+# 目前遇到的问题，
+# 1. 只要有两个 以上的进程同时使用就会卡死。
+# 2. 直接长期放后台也不行，长期sleep 也会 死。
+#
 #
 
 PROGRM="termux-tts-speak"
@@ -25,12 +31,39 @@ service(){
 	done | $PROGRM
 }
 
+openfile(){
+	exec 9<>$FIFO
+}
+
+close(){
+	exec 9<&-
+	exec 9>&-
+}
+
+
+# 解决问题2
+service2(){
+	openfile
+	trap close EXIT
+
+	local text
+	while :
+	do
+		read -t 10 -u 9 text
+		if [ $? -eq 0 ];then
+			echo "$text"
+		else
+			break
+		fi
+	done | $PROGRM
+}
+
 
 if check_service;then
 	echo "$@" > $FIFO
 else
-	service &
-	echo "start tts service pid: $!"
+	service2 &
+	#echo "start tts service pid: $!"
 	echo "$@" > $FIFO
 fi
 
