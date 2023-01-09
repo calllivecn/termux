333815ce0d09445d79be5b62ad737f2cec43ed73
calllivecn <calllivecn@outlook.com>
2023-01-09 17:09:25 +0800

    update termux-backup.sh

--- bin/termux-backup.sh
+++ bin/termux-backup.sh
@@ -1,26 +1,54 @@
 #!/bin/bash
 
-set -xe
+set -e
+
+FIFO=$(mktemp -u)
+
+DATE=$(date +%F_%X)
+
+FILENAME="termux-backup-${TERMUX_HOSTNAME}-${DATE}.tza"
+FILENAME_SHA256="${FILENAME}.sha256"
+
+
+OUTFILE="${HOME}/storage/shared/${FILENAME}"
+OUTFILE_SHA256="${HOME}/storage/shared/${FILENAME_SHA256}"
+
+errror_exit_clear(){
+	rm -vf "${OUTFILE}"
+	rm -vf "${OUTFILE_SHA256}"
+}
 
 safe_exit(){
 	rm -vf "$FIFO"
 }
 
-#trap safe_exit ERR EXIT SIGINT SIGTERM
+trap safe_exit EXIT
 
-sdcard="/storage/emulated/0"
+trap error_exit_clear SIGTERM SIGINT ERR
 
-OUTFILE="termux-backup-$(date +%F).tar.gz"
-OUTFILE_sha256="termux-backup-$(date +%F).tar.gz.sha256"
+mkfifo "$FIFO"
 
-#FIFO=$(mktemp -u)
-#mkfifo "$FIFO"
+#apt clean
 
-apt clean
+#SYS="--exclude=usr/tmp/* --exclude=usr/run/* --exclude=usr/var/run/*"
+#USER_EXCLUDE="--exclude=home/storage --exclude */__pycache__"
 
-SYS="$PREFIX/usr"
+if [ "$1"x = x ];then
+	echo "Usage: <password>"
+	echo "Usage: <password> [prompt]"
+	exit 1
 
-HOME="home"
+elif [ -n "$1" ] && [ "$2"x = x ];then
+	time { termux-backup --ignore-read-failure - | zstd -T0 -c | crypto.py -i - -o - -k "$2" | tee "$OUTFILE" |sha256sum |awk -v filename="$FILENAME" '{print $1,filename}' |tee "$OUTFILE_SHA256"; }
 
-tar -C $PREFIX --exclude $SYS --exclude $HOME --exclude */__pycache__ -zcvO . | tee "$sdcard/$OUTFILE" |sha256sum |awk -v filename="$OUTFILE" '{print $1,filename}' |tee "$sdcard/$OUTFILE_sha256"
+elif [ -n "$1" ] && [ -n "$2" ];then
+	time { termux-backup --ignore-read-failure - | zstd -T0 -c | crypto.py -i - -o - -k "$1" -p "$2" | tee "$OUTFILE" |sha256sum |awk -v filename="$FILENAME" '{print $1,filename}' |tee "$OUTFILE_SHA256"; }
+
+else
+	echo "未知选项"
+	echo "Usage: <password>"
+	echo "Usage: <password> [prompt]"
+	exit 1
+fi
+
 
