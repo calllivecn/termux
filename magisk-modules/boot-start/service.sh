24e0d768b8d2a434f65b94501d60474e1c3e4860
calllivecn <calllivecn@outlook.com>
2023-01-12 03:42:42 +0800

    small fix

--- magisk-modules/boot-start/service.sh
+++ magisk-modules/boot-start/service.sh
@@ -3,9 +3,11 @@
 
 CWD=${0%/*}
 
+LOG=${CWD}/zx.logs
+
 until [ "$(getprop sys.boot_completed)"x = 1x ];
 do
-	echo "$(date +%F_%X): waiting ..." >> ${CWD}/zx.logs
+	echo "$(date +%F_%X): waiting ..." >> "$LOG"
 	sleep 1
 done
 
@@ -14,14 +16,14 @@ MSG="$(date +%F_%X): boot_completed=1"
 
 echo $MSG 
 
-echo $MSG >> ${CWD}/zx.logs
+echo $MSG >> "$LOG"
 echo $MSG >> /data/adb/zx.logs
 
 # 启动 *.sh
 for f in ${CWD}/shells/*.sh;
 do
 	if [ -r $f ];then
-		echo "启动 ${f} ..."
+		echo "启动 ${f} ..." >> "$LOG"
 		sh "$f" &
 	fi
 done
--- magisk-modules/powerOnBoot/post-fs-data.sh
+++ magisk-modules/powerOnBoot/post-fs-data.sh
@@ -7,3 +7,12 @@
 # post-fs-data 里需要使用resetprop -n 
 
 resetprop -n sys.boot_from_charger_mode 1
+
+
+# on charger
+# 
+#   class_start charger #这段自带不用删也可以,而且听说某些系统需要先进入charger流程，否则会卡第一屏
+# 
+#   setprop sys.powerctl reboot #添加进这段
+
+#resetprop -n sys.powerctl reboot
