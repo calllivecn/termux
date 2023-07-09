d22b8624d2c571f3e1feec59b7606b96948e465e
calllivecn <c-all@qq.com>
2023-07-10 00:43:31 +0800

    update

--- scrcpy-sh/scrcpy-android-self.sh
+++ scrcpy-sh/scrcpy-android-self.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # machive="localhost:15555" 使用localhost 连接 打不开scrcpy 图形界面。不知道原因
-machive="10.1.2.6:15555"
 machive="127.0.0.1:15555"
 
 
@@ -22,17 +21,21 @@ kill_exit(){
 	fi
 }
 
-echo '#!/bin/bash' | tee adb.sh
-echo 'adb -P 16666 "$@"' | tee -a adb.sh
-chmod +x ./adb.sh
-#export ADB_TRACE=1
-export ADB="./adb.sh"
+adb_tmp=$(mktemp -p /tmp adb-XXXXX.sh)
 
-trap 'rm -v ./adb.sh' EXIT
+echo '#!/bin/bash' > "$adb_tmp"
+echo 'adb -P 16666 "$@"' >> "$adb_tmp"
+
+chmod +x "$adb_tmp"
+
+#export ADB_TRACE=1
+export ADB="$adb_tmp"
+
+trap "rm -v ${adb_tmp}" EXIT
 
 #scrcpy -S -s "${machive}" --power-off-on-close --show-touches --render-driver=software -b 2M --max-fps 8 --max-size 800
-scrcpy -S -s "${machive}" --power-off-on-close --show-touches --render-driver=software --max-fps 30 --max-size 800
-#scrcpy -S -s "${machive}" --power-off-on-close --show-touches --render-driver=metal -b 4M --max-fps 30 --max-size 800
+# v2.1
+scrcpy -S -s "${machive}" --power-off-on-close --show-touches --no-audio --render-driver=software --max-fps 30 --max-size 800
 
 kill_exit
 
