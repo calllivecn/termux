5388516753a73ff0d05f46d66116bfa38713edfe
calllivecn <c-all@qq.com>
2023-07-12 01:08:38 +0800

    测试。。。

--- magisk-modules/boot-start/service.sh
+++ magisk-modules/boot-start/service.sh
@@ -1,23 +1,36 @@
 # date 2023-01-11 18:27:03
 # author calllivecn <c-all@qq.com>
 
-CWD=${0%/*}
-LOGS=${CWD}/logs
+# 当前模块目录
+CWD=$(cd "$(dirname "${0}")";pwd)
+
+# 找到/data/adb/ 放日志. CWD 的上级目录
+LOG_DIR=$(cd ${CWD}/.. ;pwd)
+LOG_DIR=$(cd ${LOG_DIR}/.. ;pwd)
+
+LOGS=${LOG_DIR}/logs
 BOOT_LOG=${LOGS}/boot.log
 
 if [ ! -d $LOGS ];then
     mkdir -p $LOGS
+    chmod 0750 $LOGS
 fi
+
+log(){
+    local d=$(date +%F-%R:%S)
+    # ui_print 只在安装时会在app 日志里输出
+    #ui_print "$d: $@"
+    echo "$d: $@" >> "$BOOT_LOG"
+}
+
 
 until [ "$(getprop sys.boot_completed)"x = 1x ];
 do
-	echo "$(date +%F_%X): waiting ..." >> "$BOOT_LOG"
-	sleep 3
+	log "waiting ..."
+	sleep 5
 done
 
-MSG="$(date +%F_%X): boot_completed=1"
-
-echo $MSG >> "$BOOT_LOG"
+log "boot_completed=1"
 
 # 启动 *.sh
 for f in ${CWD}/shells/*.sh;
@@ -25,7 +38,7 @@ do
 	if [ -r $f ];then
         shell=${f##*/}
         prefix_shell=${shell%.sh}
-		echo "启动 ${shell} ..." >> "$BOOT_LOG"
+		log "启动 ${shell} ..."
 		sh "$f" >> "${CWD}/logs/${prefix_shell}.log" 2>&1 &
 	fi
 done
--- magisk-modules/boot-start/shells/20-termux-env.sh
+++ magisk-modules/boot-start/shells/20-termux-env.sh
@@ -1,51 +1,80 @@
 # 启动 termux env
 
+CWD=$(cd $(dirname "${0}");pwd)
+cd ${CWD}
+
+log(){
+    echo "$(date +%F_%X): $@"
+    echo "$(date +%F_%X): $@" >> $TMUX_BOOT_LOG >/dev/null 2>&1
+}
+
+notexists_make(){
+    local d="$1"
+    if [ -d "$d" ];then
+        :
+    else
+        mkdir -vp "$d"
+    fi
+}
+
 
 TERMUX_HOME="/data/data/com.termux/files"
+# 检测5分钟
+timeout="false"
+for i in $(seq 1 60)
+do
+    if [ -d "$TERMUX_HOME" ];then
+        timeout="true"
+        break
+    else
+        log "等待目录挂载上来。。。"
+        sleep 5
+    fi
+done
 
-if [ -d "$TERMUX_HOME/usr/bin" ];then
-    export PATH=$PATH:$COM_TERMUX/usr/bin
+if [ "$timeout"x = falsex ];then
+    log "可能有问了，${TERMUX_HOME} 目录一直没挂载上来"
+    exit 1
 fi
 
-CWD=$(cd $(dirname "${0}");pwd)
 
-cd ${CWD}
-echo ${CWD}
+# 启动太早了？目录还没挂载上来？
+if [ -d "$TERMUX_HOME/usr/bin" ];then
+    export PATH=$PATH:$TERMUX_HOME/usr/bin
+else
+    log "termux app 没有安装？"
+    exit 1
+fi
 
-# 检测是否已安装 bash tmux
+log ${CWD}
 
 if type bash && type tmux;then
     :
 else
-    echo "需要安装bash 才能正确引导 termux env"
+    log "需要安装bash 才能正确引导 termux 环境"
     exit 1
 fi
 
+HOME="$TERMUX_HOME/root"
+notexists_make "$HOME"
+
+SESSION="daemon4root"
+
+TMUX_SOCK="$HOME/.tmux.sock"
+
+TMUX_BOOT_LOG="$HOME/.zx/logs/boot4root.logs"
+notexists_make "$HOME/.zx/logs"
+
+
 # 查看有没有需要引导的 *.sh
-TERMUX_BOOT4ROOT="$TERMUX_HOME/.boot4root"
+TERMUX_BOOT4ROOT="$HOME/.boot4root"
 if ls $TERMUX_BOOT4ROOT/*.sh >/dev/null 2>&1 ;then
-    echo "开始引导termux for root 环境 ..."
+    log "开始引导termux for root 环境 ..."
 else
-    echo "没有需要开机从root引导的*.sh"
+    log "没有需要开机从root引导的*.sh"
     exit 0
 fi
 
-HOME="$TERMUX_HOME/root"
-
-
-SESSION="daemon"
-TMUX_SOCK="$HOME/.tmux.sock"
-TMUX_BOOT_LOG="$HOME/.zx/logs/boot.logs"
-
-if [ -d "$HOME/.zx/logs" ];then
-    :
-else
-    mkdir -vp "$HOME/.zx/logs"
-fi
-
-log(){
-    echo "$(date +%F_%X): $@" >> $TMUX_BOOT_LOG
-}
 
 # auto start
 auto_start(){
@@ -72,13 +101,11 @@ check_tmux_daemon(){
 }
 
 if check_tmux_daemon;then
-    echo "tmux session: $SESSION alread start"
+    log "tmux session: $SESSION alread start"
 else
-    echo "tmux session: $SESSION start done"
+    log "tmux session: $SESSION start done"
     tmux -S $TMUX_SOCK new -s $SESSION -d "bash"
     auto_start
 fi
-
-
 
 
