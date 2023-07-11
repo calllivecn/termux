3afae8d2dab5f182d62e815c0aecc5abcb6e97a9
calllivecn <c-all@qq.com>
2023-07-11 12:22:05 +0800

    update

--- su2termux.sh
+++ su2termux.sh
@@ -10,7 +10,5 @@ export HOME=$CWD/home
 
 unset CWD
 
-#su - u0_a156 --preserve-environment \
-#-c "/data/data/com.termux/files/usr/bin/bash --init-file $HOME/.bashrc"
+bash --init-file $HOME/.bashrc
 
-su - u0_a156 -c "/data/data/com.termux/files/usr/bin/bash --init-file $HOME/.bashrc"
