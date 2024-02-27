64b112ea6574323f83ea0e60dcd5e0c8452b7008
calllivecn <calllivecn@outlook.com>
2024-02-27 17:47:09 +0800

    完善 am start 启动app

--- am-start-app.sh
+++ am-start-app.sh
@@ -2,6 +2,9 @@
 # date 2021-09-24 16:44:24
 # author calllivecn <c-all@qq.com>
 
+
+# 模拟从图标启动app
+
 # 启动app, 不过需要root权限。
 #am start -n net.christianbeier.droidvnc_ng/.MainActivity
 am start -n com.wireguard.android/.activity.MainActivity
@@ -10,6 +13,8 @@ am start -n com.wireguard.android/.activity.MainActivity
 # 查看包的启动 MainActivity
 # 1. 切换到root。
 # 2. pm list packages |grep "$packagename"
-# 3. dumpsys package "$packagename" |less
+# 3. dumpsys package "$packagename" |grep -A 1 "android.intent.action.MAIN"
+#    or pm dump "$packagename" |grep -A 1 "android.intent.action.MAIN"
 # 4. 找到这样一行：“android.intent.action.MAIN:” 它下面一行就是了
 #         54ed60c com.termux/.app.TermuxActivity filter d428d44
+#
--- am-start-termux.sh
+++ am-start-termux.sh
@@ -2,15 +2,5 @@
 # date 2021-09-24 16:44:24
 # author calllivecn <c-all@qq.com>
 
-# 启动app, 不过需要root权限。
-#am start -n com.termux/.app.TermuxActivity
-
-# 查看包的启动 MainActivity
-# 1. 切换到root。
-# 2. pm list packages |grep "$packagename"
-# 3. dumpsys package "$packagename" |less
-# 4. 找到这样一行：“android.intent.action.MAIN:” 它下面一行就是了
-#         54ed60c com.termux/.app.TermuxActivity filter d428d44
-# install --> ~/.termux/boot/adbd
-
 am start -n com.termux/.app.TermuxActivity
+
