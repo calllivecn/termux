009e9cb79d340301d7e2c264f36445b64b57efab
calllivecn <calllivecn@outlook.com>
2026-01-14 01:08:06 +0800

    save

--- adb-ops/disable-input/disable-input-device.py
+++ adb-ops/disable-input/disable-input-device.py
@@ -4,6 +4,7 @@ import sys
 import logging
 import argparse
 import threading
+import toml
 from pathlib import Path
 
 import libevdev as ev
@@ -39,6 +40,44 @@ else:
     os.environ['LD_LIBRARY_PATH'] = str(so_path)
 """
 
+def find_device_path(devname: str = "", vid: int = -1, pid: int = -1) -> list[str]:
+
+    if devname == "" and vid == -1 and pid == -1:
+        raise ValueError(f"需要有 <devname> or <vid> <pid>")
+
+    baseinput="/dev/input"
+
+    inputs = []
+    for devnode in os.listdir(baseinput):
+        devpath = Path(baseinput) / devnode
+        if not devpath.is_dir():
+            inputs.append(devpath)
+    
+
+    devs: list[ev.Device] = []
+    for devnode in inputs:
+        with open(devnode, "rb") as fp:
+            try:
+                # 以只读方式打开设备
+                device = ev.Device(fp)
+
+                # 获取设备信息
+                vid = device.id["vendor"]
+                pid = device.id["product"]
+                bus = device.id["bustype"]
+
+                if devname != "":
+                    if devname == device.name:
+
+            except OSError as e:
+                logger.warning(f"{devnode=} [Error: {e}]")
+            except Exception as e:
+                logger.warning(f"{devnode=} [Error: {e}]")
+
+    return devs
+
+
+
 
 def disable_device(path: Path):
 
@@ -54,16 +93,34 @@ def disable_device(path: Path):
 
 
 def main():
-    parse = argparse.ArgumentParser(usage="%(prog)s --help")
+    parse = argparse.ArgumentParser(usage="%(prog)s")
     parse.add_argument("--debug", action="store_true", help="开启debug日志")
-    groups = parse.add_mutually_exclusive_group(required=True)
+    parse.add_argument("--parse", action="store_true", help=argparse.SUPPRESS)
+    groups = parse.add_mutually_exclusive_group()
     groups.add_argument("--toml", action="store", help="指定配置文件")
-    groups.add_argument("devs", nargs="+", help="/dev/input/XXX")
+    groups.add_argument("--devs", nargs="*", help="/dev/input/XXX")
     
     args = parse.parse_args()
 
+    if args.parse:
+        logger.info(f"{args}")
+        sys.exit(0)
+    
+    if args.toml is None and args.devs is None:
+        parse.print_help()
+        logger.error(" 使用 配置文件 方式和 指定设备节点 方式，必须有一项。(两种方式都指定了，使用 配置文件 方式)")
+        sys.exit(1)
+    elif args.toml:
+        devs = toml.load(args.toml)
+    elif args.devs:
+        devs = args.devs
+    else:
+        logger.error("未知错误")
+
+
     if args.debug:
         logger.setLevel(logging.DEBUG)
+
     
     threads: list[threading.Thread] = []
     for dev in args.devs:
